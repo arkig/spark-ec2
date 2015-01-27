707ea7183f51daa9a4e09e385dfcf4c2d9ca51b9
Florian Verhein <florian.verhein@gmail.com>
2015-01-27 16:10:29 +1100

    fix/improve hadoop build from source - define packer path, fix bug with natives missing in /root/hadoop/

--- packer/hadoop-setup.sh
+++ packer/hadoop-setup.sh
@@ -3,17 +3,24 @@
 # Useful:
 # - https://cyberfrontierlabs.com/2014/09/30/installing-hadoop-2-5-1-on-centos-7/
 
-source ./image_variables.sh
+set -e
+set -x
+set -u
 
 # Install Maven (for building Hadoop, etc)
 cd /tmp
 wget "http://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
 tar xvzf apache-maven-${MAVEN_VERSION}-bin.tar.gz
-mv apache-maven-${MAVEN_VERSION} /opt/
+sudo mv apache-maven-${MAVEN_VERSION} /opt/
 # Edit bash profile
 echo "export M2_HOME=/opt/apache-maven-${MAVEN_VERSION}" >> ~/.bash_profile
 echo "export PATH=\$PATH:\$M2_HOME/bin" >> ~/.bash_profile
+set +u
 source ~/.bash_profile
+set -u
+
+# Prerequisites
+sudo yum install -y protobuf-compiler cmake openssl-devel
 
 # Install exact required version of protobuf
 cd /tmp
@@ -25,25 +32,30 @@ make
 make check
 sudo make install
 
-# Build Hadoop to install native libs
-sudo yum install -y protobuf-compiler cmake openssl-devel
-sudo mkdir /root/hadoop-native
+# Build Hadoop
 cd /tmp
 wget "http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}-src.tar.gz"
 tar xvzf hadoop-${HADOOP_VERSION}-src.tar.gz
 cd hadoop-${HADOOP_VERSION}-src
+# Take care of possibility that we have multiple versions.
+export HADOOP_PROTOC_PATH="/usr/local/bin/protoc"
 mvn package -Pdist,native -DskipTests -Dtar
-sudo cp hadoop-dist/target/hadoop-${HADOOP_VERSION}/lib/native/* /root/hadoop-native
 
 # Keep build so we can use it later
-sudo rm -rf /root/hadoop
-sudo mv hadoop-dist/target/hadoop-${HADOOP_VERSION} /root/hadoop
-
-# hadoop user ownership/setup??
+rm -rf /root/hadoop
+mv hadoop-dist/target/hadoop-${HADOOP_VERSION} /root/hadoop
 
 # Install Snappy lib (for Hadoop)
 sudo yum install -y snappy
-sudo ln -sf /usr/lib64/libsnappy.so.1 /root/hadoop-native/.
+sudo ln -sf /usr/lib64/libsnappy.so.1 /root/hadoop/lib/native/.
+
+# Grab native libs only for later use in modules (in case we download a different hadoop)
+mkdir /root/hadoop-native
+cp /root/hadoop/lib/native/* /root/hadoop-native
+
+
+# hadoop user ownership/setup??
+
 
 # Clean up
 rm -rf /tmp/*
