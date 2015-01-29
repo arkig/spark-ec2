9a76acb455158cff79b9db9d57bd33091c200740
Florian Verhein <florian.verhein@gmail.com>
2015-01-29 16:39:52 +1100

    fix local disks and mounts (hopefully...)

--- setup-slave.sh
+++ setup-slave.sh
@@ -21,32 +21,60 @@ HOSTNAME=$PRIVATE_DNS  # Fix the bash built-in hostname variable too
 
 echo "Setting up slave on `hostname`..."
 
-# Work around for R3 instances without pre-formatted ext3 disks
 instance_type=$(curl http://169.254.169.254/latest/meta-data/instance-type 2> /dev/null)
-if [[ $instance_type == r3* ]]; then
-  # Format & mount using ext4, which has the best performance among ext3, ext4, and xfs based
-  # on our shuffle heavy benchmark
-  EXT4_MOUNT_OPTS="defaults,noatime,nodiratime"
-  rm -rf /mnt*
-  mkdir /mnt
-  # To turn TRIM support on, uncomment the following line.
-  #echo '/dev/sdb /mnt  ext4  defaults,noatime,nodiratime,discard 0 0' >> /etc/fstab
-  mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sdb
-  mount -o $EXT4_MOUNT_OPTS /dev/sdb /mnt
+echo "Instance type: $instance_type"
 
-  if [[ $instance_type == "r3.8xlarge" ]]; then
-    mkdir /mnt2
-    # To turn TRIM support on, uncomment the following line.
-    #echo '/dev/sdc /mnt2  ext4  defaults,noatime,nodiratime,discard 0 0' >> /etc/fstab
-    mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/sdc
-    mount -o $EXT4_MOUNT_OPTS /dev/sdc /mnt2
+EXT_MOUNT_OPTS="defaults,noatime,nodiratime"
+function setup_instance_volume {
+
+  device=$1
+  mount_point=$2
+
+  if [[ -e $device ]]; then
+
+    if grep -qs "$mount_point" /proc/mounts; then
+      umount $mount_point
+    fi
+
+    rm -rf $mount_point
+    mkdir $mount_point
+
+    # Check if device is already formatted
+    if ! blkid $device; then
+      echo "Formatting $device..."
+
+      # To turn TRIM support on, uncomment the following line.
+      #echo "$device $mount_point  ext4  defaults,noatime,nodiratime,discard 0 0" >> /etc/fstab
+
+      # Format using ext4, which has the best performance among ext3, ext4, and xfs based
+      # on our shuffle heavy benchmark
+      mkfs.ext4 -E lazy_itable_init=0 $device
+    else
+      echo "$device appears to be formatted. Leaving as is."
+      blkid $device
+    fi
+
+    echo "Mounting $device onto $mount_point..."
+    mount -o $EXT_MOUNT_OPTS $device $mount_point
+
+    # Make data dirs writable by non-root users, such as CDH's hadoop user
+    chmod -R a+w $mount_point
+
+  else
+    echo "$device does not exist."
   fi
-fi
+}
 
-# Mount options to use for ext3 and xfs disks (the ephemeral disks
-# are ext3, but we use xfs for EBS volumes to format them faster)
+# This will hopefully work for most hvm instance types.
+echo "Setting up ephemeral instance volumes..."
+setup_instance_volume /dev/xvdb /mnt
+setup_instance_volume /dev/xvdc /mnt2
+setup_instance_volume /dev/xvdd /mnt3
+setup_instance_volume /dev/xvde /mnt4
+
+
+
 XFS_MOUNT_OPTS="defaults,noatime,nodiratime,allocsize=8m"
-
 function setup_ebs_volume {
   device=$1
   mount_point=$2
@@ -72,10 +100,13 @@ function setup_ebs_volume {
         chmod -R a+w $mount_point
       fi
     fi
+  else
+    echo "$device does not exist."
   fi
 }
 
 # Format and mount EBS volume (/dev/sd[s, t, u, v, w, x, y, z]) as /vol[x] if the device exists
+echo "Setting up EBS volumes..."
 setup_ebs_volume /dev/sds /vol0
 setup_ebs_volume /dev/sdt /vol1
 setup_ebs_volume /dev/sdu /vol2
@@ -91,8 +122,6 @@ if [[ -e /vol3 && ! -e /vol ]]; then
   ln -s /vol3 /vol
 fi
 
-# Make data dirs writable by non-root users, such as CDH's hadoop user
-chmod -R a+w /mnt*
 
 # Remove ~/.ssh/known_hosts because it gets polluted as you start/stop many
 # clusters (new machines tend to come up under old hostnames)
